9185c0f8e90a79808e64a8fcdce4e6a069af47ca
ntuvera <ntuvera@gmail.com>
2014-06-13 16:33:54 -0400

    change incoming_mails_controller

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,11 +1,12 @@
 class IncomingMailsController < ApplicationController
-  require 'mail'
   skip_before_filter :verify_authenticity_token
 
   def create
-    message = Mail.new(params[:message])
-    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
-    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
+    Rails.logger.log params[:envelope][:to] # print the to field to the logs
+    Rails.logger.log params[:subject] # print the subject to the logs
+    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
+    Rails.logger.log params[:html] # print the html decoded body to the logs if present
+    Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
 
     # Do some other stuff with the mail message
 
