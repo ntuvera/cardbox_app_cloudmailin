628b01ba304c516aa73a44ea23ac321944aafaee
ntuvera <ntuvera@gmail.com>
2014-06-13 16:25:15 -0400

    remove and comment out mailgun code

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,4 @@
 class Email < ActiveRecord::Base
-  attr_acessible :attachment_name
-  mount_uploader :attachment_name, ImageUploader
+  # attr_acessible :attachment_name
+  # mount_uploader :attachment_name, ImageUploader
 end
