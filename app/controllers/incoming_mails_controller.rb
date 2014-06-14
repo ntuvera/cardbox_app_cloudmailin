e46932a23970f9e84da1486d66dc0a0ea452e002
atrapp <andrea.trapp@gmx.net>
2014-06-14 13:11:22 -0400

    IncomingMailController checks if User email exists

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,18 +2,39 @@ class IncomingMailsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
     Rails.logger.info params
-    message = Email.new(
-      :to => params[:envelope][:to],
-      :sender => params[:envelope][:from],
-      :subject => params[:headers]['Subject'],
-      :message => params[:plain],
-      :attachment_url => params["attachments"]["0"]["url"]
-    )
-    if message.save
-      render :text => 'Success', :status => 200
+
+    if User.find_by(email: params[:envelope][:from]) 
+
+
+      message = Email.new(
+        :to => params[:envelope][:to],
+        :sender => params[:envelope][:from],
+        :subject => params[:headers]['Subject'],
+        :message => params[:plain],
+        :attachment_url => params["attachments"]["0"]["url"]
+      )
+
+  
+
+
+
+
+   
+
+
+      if message.save
+        render :text => 'Success', :status => 200
+      else
+        render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
+      end
+
     else
-      render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
+      render :text => "No User", :status => 422, :content_type => Mime::TEXT.to_s
+        
+   
     end
+
+
   end
 end
 
