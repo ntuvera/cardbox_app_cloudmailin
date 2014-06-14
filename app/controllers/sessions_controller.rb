6ee5ff181ff3f5a98fa3b2a4425d1f548db0a149
ntuvera <ntuvera@gmail.com>
2014-06-14 15:59:37 -0400

    create signup, login, profile, logout? loop, PLUS handydandy tmp/cache crap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to root_path
+      redirect_to profile_path
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
 
-  get '/messages/send' => 'emails#send_simple_message'
+  get '/messages/send'     => 'emails#send_simple_message'
   post '/messages/receive' => 'emails#receive'
 
-  post '/incoming_mails' => 'incoming_mails#create'
+  post '/incoming_mails'   => 'incoming_mails#create'
 
 
   resources :emails
 
-  resources :users
+  get '/signup'   => 'users#new', as: 'signup'
+  post '/users'   => 'users#create', as: 'users'
+  get '/profile'  => 'users#profile', as: 'profile'
+
+  get '/login'      => 'sessions#new', as: 'login'  #can refer to as login_path
+  post '/sessions'  => 'sessions#create', as: 'session'
+  delete '/logout'  => 'sessions#destroy', as: 'logout'  #can refer to as logout_path
 
   resources :cards
 
