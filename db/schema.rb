fe3d6ac6687ddf2fa78b1c682f5c349d4d821cf1
paulh1am <pshiam@gmail.com>
2014-06-14 19:11:57 -0400

    added contact model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614201558) do
+ActiveRecord::Schema.define(version: 20140614231019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20140614201558) do
   create_table "cards", force: true do |t|
     t.integer  "user_id"
     t.integer  "email_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "linkedin_id"
+    t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
