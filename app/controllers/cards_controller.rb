90c788a96c5892b43828efa4e3b48cf2577b35a4
atrapp <andrea.trapp@gmx.net>
2014-06-14 12:55:20 -0400

    created User model and controller

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,41 @@
+class CardsController < ApplicationController
+
+  def index
+    @cards = Card.all
+  end
+
+  def show
+    @card = Card.find(card_params[:id])
+  end
+
+  def new
+    @card = Card.new #form partial
+    #show a form
+  end
+
+  def create
+    @card = Card.create(card_params)
+    redirect_to cards_path
+  end
+
+  def edit
+    @card = Card.find(card_params[:id])
+  end
+
+  def update
+    edited_card = Card.find(card_params[:id])
+    edited_card.update(card_params)
+  end
+
+  def destroy
+    Card.delete(card_params[:id])
+    redirect_to card_path
+  end
+
+private
+
+  def card_params
+    params.require(:card).permit(:sender, :subject, :message, :attachment_url, :to)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new #form partial
+    #show a form
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    edited_user = User.find(params[:id])
+    edited_user.update(user_params)
+  end
+
+  def destroy
+    User.delete(params[:id])
+    redirect_to users_path
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+end
