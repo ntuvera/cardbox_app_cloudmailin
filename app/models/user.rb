d9082a2205e8c5a891c0c96abcfe92d1d6bdf86a
atrapp <andrea.trapp@gmx.net>
2014-06-17 17:53:17 -0400

    workaround with create account from linkedin... check password on create commented because of error

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   #validates_presence_of :password, on: :create
   validates_presence_of :email, on: :create
   validates_uniqueness_of :email
-  validates_presence_of :password, on: :create
+  # validates_presence_of :password, on: :create
   # validates :password, length: {within: 8..16, too_short: "too short. Your password needs mininum 8 characters.", too_long: "too long. Your password exceeds the maximum length of 16 characters."}
   # validates :email, presence: true, confirmation: true, uniqueness: true, email: true
   # validates :terms_of_service, acceptance: true
@@ -19,10 +19,11 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       # user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      #binding.pry
+     
       user.email = auth.info.email
       user.crypted_password = ENV["crypted_password"]
       user.salt = ENV["salt"]
+    
       user.save!
     end
   end
